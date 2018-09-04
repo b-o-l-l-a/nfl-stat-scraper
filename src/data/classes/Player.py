ee47ad6011d2c2c3152b7836cc9a2454e90bde4d
Gregory Bolla <greg.bolla@Gregorys-MacBook-Pro.local>
2018-09-04 05:40:31 -0600

    can handle games and metadata about player

--- src/data/classes/Player.py
+++ src/data/classes/Player.py
@@ -1,41 +1,44 @@
-from classes.Positions import OffLineman, Quarterback, RunningBack, WideReceiver, TightEnd, DefLineman, Linebacker, DefBack, Kicker, Punter
+from bs4 import BeautifulSoup, NavigableString
+import datetime
+import re
+import os
 
-class PlayerGame():
-    def __init__(self, Team, player_name, player_link, position):
+from classes.Positions import OffLineman, Quarterback, RunningBack, WideReceiver, TightEnd, DefLineman, Linebacker, DefBack, Kicker, Punter
+from utils import get_html, get_config
+
+class Player():
+    def __init__(self, player_name, player_link,  Team = None, position = None):
         
         self.min_snap_perc = .10
         self.name = player_name
-        self.player_link = player_link   
-        self.game_html_page = Team.game_html_page
-        self.season = Team.season
-        self.week = Team.week
-        self.team = Team.team
-        self.team_abbrev = Team.team_abbrev
-        self.base_url = Team.base_url
+        self.player_link = player_link  
+        
+        config = get_config(os.getcwd())
+        self.base_url = config['base_url']
         self.full_player_url = self.base_url + player_link
         self.standardized_position_dict = {
-            "OL" : {"eligible_positions" : ["G", "T", "C", "LS"], 
+            "OL" : {"eligible_positions" : ["G", "T", "C", "LS", "OT", "OG", "OL","G/C", "G-C", "T-G", "G-T", "C-G", "G,C", "C,G", "G,T", "T,G"], 
                    "class": OffLineman,
                    "side" : "offense"},
             "QB" : {"eligible_positions" : ["QB"], 
                    "class": Quarterback,
                    "side" : "offense"},
-            "WR" : {"eligible_positions" : ["WR"],
+            "WR" : {"eligible_positions" : ["WR", "PR-WR", "WR/RB"],
                    "class": WideReceiver,
                    "side" : "offense"},
-            "TE" : {"eligible_positions" : ["TE"],
+            "TE" : {"eligible_positions" : ["TE", "LS,TE", "TE-C"],
                    "class": TightEnd,
                    "side" : "offense"},
-            "RB" : {"eligible_positions" : ["RB", "FB"],
+            "RB" : {"eligible_positions" : ["RB", "FB", "FB-LB", "HB"],
                    "class": RunningBack,
                    "side" : "offense"},
-            "DB" : {"eligible_positions" : ["SS", "FS", "CB", "DB"],
+            "DB" : {"eligible_positions" : ["SS", "FS", "CB", "DB", "S"],
                    "class": DefBack,
                    "side" : "defense"},
-            "LB" : {"eligible_positions" : ["LB", "OLB", "MLB"],
+            "LB" : {"eligible_positions" : ["LB", "OLB", "ILB", "MLB", "LB-DE"],
                    "class": Linebacker,
                    "side" : "defense"},
-            "DL" : {"eligible_positions" : ["DT", "DL", "NT", "DE"],
+            "DL" : {"eligible_positions" : ["DT", "DL", "NT", "DE", "NT-DT", "DT-NT", "DE-LB", "DT/LB", "DE-C", "DE-DT", "DT-DE"],
                    "class": DefLineman,
                    "side" : "defense"},
             "K" : {"eligible_positions" : ["K"],
@@ -46,10 +49,42 @@ class PlayerGame():
                    "side" : "special_teams"}
         }
         
+        
+        if Team is not None:
+            self.game_html_page = Team.game_html_page
+            self.season = Team.season
+            self.week = Team.week
+            self.team = Team.team
+            self.team_abbrev = Team.team_abbrev
+            self.base_url = Team.base_url
+        
+        if position is None:
+            self.player_page = get_html(self.full_player_url)
+            self.meta_div = self.player_page.find("div", {"itemtype" : "https://schema.org/Person"})
+            position = self.get_position_from_player_page(self.meta_div)
+        
         self.standardized_pos = [k for k, v in self.standardized_position_dict.items() \
                                  if position in v["eligible_positions"]][0]
         self.player_class = self.standardized_position_dict[self.standardized_pos]["class"]
         self.side = self.standardized_position_dict[self.standardized_pos]['side']
+
+    def get_position_from_player_page(self, metadata_div):
+        pattern = re.compile(r'Position')
+        if metadata_div.find('strong', text=pattern) is None:
+            if self.name == "Virgil Green":
+                position = "TE"
+            elif self.name == "Vincent Brown":
+                position = "WR"
+            elif self.name == "Michael Davis":
+                position = "DB"
+        else:
+          
+            tag = metadata_div.find('strong', text=pattern).parent
+            for child in tag:
+                if isinstance(child, NavigableString) and ":" in child:
+                    position = child.split(":")[-1].strip()
+            
+        return position
 
     def get_player_summary(self):
     
@@ -137,3 +172,84 @@ class PlayerGame():
         
         pos_stats_flg = all([player_game_stats_flg, perc_snaps_flg])
         return pos_stats_flg
+    
+    def get_player_data(self):
+        
+        player_data = {}
+        player_data['player_link'] = self.player_link
+        player_data['player_name'] = self.name
+        player_data['position'] = self.standardized_pos
+        
+        player_metadata = self.get_player_metadata()
+        player_combine_data = self.get_combine_data()
+        
+        player_data.update(player_metadata)
+        player_data.update(player_combine_data)
+        
+        return player_data
+    
+    def get_combine_data(self):
+        
+        combine_tbl = self.player_page.find("table", {"id": "combine"})
+        combine_dict = {}
+        
+        if combine_tbl is not None:
+            combine_row = combine_tbl.tbody.tr
+            combine_yr = combine_row.find("th", {"data-stat" : "year_id"}).a.string
+            combine_height = combine_row.find("td", {"data-stat" : "height"}).string
+            combine_weight = combine_row.find("td", {"data-stat" : "weight"}).string
+            combine_forty = combine_row.find("td", {"data-stat" : "forty_yd"}).string
+            combine_bench = combine_row.find("td", {"data-stat" : "bench_reps"}).string
+            combine_broad_jump = combine_row.find("td", {"data-stat" : "broad_jump"}).string
+            combine_shuttle = combine_row.find("td", {"data-stat" : "shuttle"}).string
+            combine_cone = combine_row.find("td", {"data-stat" : "cone"}).string
+            combine_vert = combine_row.find("td", {"data-stat" : "vertical"}).string
+            
+            combine_dict['combine_year'] = combine_yr
+            combine_dict['height'] = combine_height
+            combine_dict['weight'] = combine_weight
+            combine_dict['forty_yd'] = combine_forty
+            combine_dict['combine_bench'] = combine_bench
+            combine_dict['combine_broad_jump'] = combine_broad_jump
+            combine_dict['combine_shuttle'] = combine_shuttle
+            combine_dict['combine_cone'] = combine_cone
+            combine_dict['combine_vert'] = combine_vert
+            
+        return combine_dict
+    def get_player_metadata(self):
+        
+        player_metadata_dict = {}
+        player_page = self.player_page
+        birthdate_string = player_page.find("span", {"itemprop":"birthDate"})['data-birth']
+        birthdate = datetime.datetime.strptime(birthdate_string, '%Y-%m-%d')
+        player_metadata_dict['birthdate'] = birthdate
+        
+        draft_pick = self.get_draft_pick()
+        player_metadata_dict['draft_pick'] = draft_pick        
+        
+        return player_metadata_dict
+    
+    def get_draft_pick(self):
+        meta_div = self.meta_div
+        draft_pattern = re.compile(r'Draft')        
+        
+        if meta_div.find('strong', text=draft_pattern) is None:
+            draft_pick = None
+        else:
+            draft_tag = meta_div.find('strong', text=draft_pattern).parent
+        
+            for child in draft_tag.children:
+
+                # check to see if child is a string, and check to see if there are parentheses within that string
+                if isinstance(child, NavigableString) and re.search(r'\((.*?)\)',child) is not None:
+
+                    re_draft_pick = re.search(r'\((.*?)\)',child)            
+                    draft_pick_string = re_draft_pick.group(1)
+
+                    # in the string that specifies draft pick, grab the number before ordinal string
+                    draft_pick = re.search(r'.+?(?=[st|nd|rd|th])', draft_pick_string).group(0)
+                    draft_pick = int(draft_pick)
+                    
+        return draft_pick
+        
+        
