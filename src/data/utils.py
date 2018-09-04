254a426c1a2c402617b475dda2b156107577c0a7
Gregory Bolla <greg.bolla@Gregorys-MacBook-Pro.local>
2018-09-04 05:45:10 -0600

    drop dict to csv fxn

--- src/data/utils.py
+++ src/data/utils.py
@@ -4,6 +4,7 @@ import requests
 import re
 import time
 import json
+import pandas as pd
 
 def get_config(home_dir):
     path = "{}/config.json".format(home_dir)
@@ -22,3 +23,25 @@ def get_html(url):
     time.sleep(wait_time)
     
     return soup
+
+def dict_to_csv(dict_to_append, path, file_name):
+
+    for key, val in dict_to_append.items():
+        dict_to_append[key] = [val]
+    
+    df_to_append = pd.DataFrame(data=dict_to_append)
+    
+    csv_name = "{}.csv".format(file_name)
+    full_path = os.path.join(path, csv_name)
+
+    if os.path.isfile(full_path):
+        df = pd.read_csv(full_path)
+        df = df.drop_duplicates()
+    else:
+        df = pd.DataFrame()
+
+    df = df.append(df_to_append, ignore_index = True)
+    df = df.drop_duplicates()
+    df.to_csv(full_path, index = False)
+    
+    return
